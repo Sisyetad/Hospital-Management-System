f0a63c05aeeaff294194f7779992af919e6de49f
Sisyetad <sisaytadewos@gmail.com>
2026-01-29 22:52:39 +0300

    notification for each user when created

--- hospitalmanagementsystem/User/Application/task.py
+++ hospitalmanagementsystem/User/Application/task.py
@@ -2,8 +2,9 @@ from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
 
-@shared_task
-def send_user_created_confirmation_email(user_email):
+@shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={"max_retries": 3})
+def send_user_created_confirmation_email(self, user_email):
     subject = "User is created!"
     message = f"You can signup now your User Email {user_email} is already created."
-    return send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user_email])
+    return send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user_email], 
+                    fail_silently=False)
